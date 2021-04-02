86c14d70937fc8a8c2075c0999e36740aeb0d3d2
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-04-02 17:42:05 -0600

    Spring cleaning~~~

---
+++ KD-Tree C++/src/main.cpp
@@ -0,0 +1,47 @@
+/*
+TODO LIST
+1) Insert
+2) Search (, and return)
+*/
+
+
+#include "KD.h"
+#include <fstream>
+
+void extractInformation(KD*);
+
+int main()
+{
+	KD* tree = new KD();
+
+	extractInformation(tree);
+
+	tree->printNodes();
+
+	return 0;
+}
+
+void extractInformation(KD* tree)
+{
+	std::ifstream inputStream("./input/input.txt");
+
+	int first, second; // Grabbing Coordinates
+	char comma; // Comma catching
+	std::vector<int> newData; // Encapsulation
+
+
+	if (inputStream.is_open())
+	{
+		while (!inputStream.eof())
+		{
+			inputStream >> first >> comma >> second;
+
+			newData.push_back(first);
+			newData.push_back(second);
+
+			tree->add(newData);
+
+			newData.clear();
+		}
+	}
+}
